9cc97f3f9cd42bd2f464449d9c5da273f0d42a7d
timeester3648 <weteringt@gmail.com>
2023-11-11 16:47:51 +0100

    Update

--- premake5.lua
+++ premake5.lua
@@ -17,18 +17,57 @@ project "Skia"
 
 	includedirs {
 		"%{IncludeDir.icu}",
+		"%{IncludeDir.vma}",
 		"%{IncludeDir.skia}",
+		"%{IncludeDir.zlib}",
+		"%{IncludeDir.brotli}",
+		"%{IncludeDir.libwebp}",
 		"%{IncludeDir.harfbuzz}",
 		"%{IncludeDir.freetype}",
-		"%{IncludeDir.vulkan_headers}"
+		"%{IncludeDir.spirv_tools}",
+		"%{IncludeDir.spirv_cross}",
+		"%{IncludeDir.spirv_headers}",
+		"%{IncludeDir.vulkan_headers}",
+
+		"./src/gpu/vk/vulkanmemoryallocator/"
 	}
 
 	excludes { 
-		"./src/**/*dawn*",
+		"./src/**/ganesh/gl/**",
+		"./src/**/ganesh/d3d/**",
+
 		"./src/**/*mozilla*",
+		"./src/**/*mozalloc*",
+
+		"./src/codec/*Png*",
+		"./src/codec/*Raw*",
+		"./src/codec/*Jpeg*",
+		"./src/codec/*Avif*",
+		"./src/codec/*Wuffs*",
+		
+		"./src/encode/*SkPngEncoderImpl*",
+		"./src/encode/*SkJpegEncoderImpl*",
+		"./src/encode/*SkJPEGWriteUtility*",
+		"./src/encode/*SkWebpEncoder_none*",
+		"./src/encode/*SkJpegGainmapEncoder*",
+
+		"./src/pdf/*SkDocument_PDF_None*",
+
+		"./src/ports/*NDK*",
+		"./src/ports/*SkFontMgr_win*",
+		"./src/ports/*SkFontMgr_empty_factory*",
+		"./src/ports/*SkFontMgr_custom_empty_factory*",
+		"./src/ports/*SkFontMgr_custom_directory_factory*",
+
+		"./src/**/*dawn*",
+		"./src/**/*vello*",
+		"./src/**/*fuchsia*",
 		"./src/**/*android*",
+		"./src/**/*fontations*",
 		"./src/**/*fontconfig*",
 		"./src/**/*libjpegturbo*",
+
+		"./src/xml/**",
 
 		"./src/gpu/vk/vulkanmemoryallocator/VulkanMemoryAllocatorWrapper.cpp"
 	}
@@ -36,11 +75,25 @@ project "Skia"
 	defines {
 		"SK_VULKAN",
 		"SK_USE_VMA",
-		"SK_GRAPHITE"
+
+		"SK_GANESH",
+		"SK_GRAPHITE",
+
+		"SK_TYPEFACE_FACTORY_FREETYPE",
+		"SK_FREETYPE_MININUM_RUNTIME_VERSION_IS_BUILD_VERSION",
+
+		"SKIA_IMPLEMENTATION=1"
 	}
 
 	filter "system:windows"
-		disablewarnings { "4244" }
+		disablewarnings { "4244", "4267", "4291" }
 		defines { "SK_BUILD_FOR_WIN", "_CRT_SECURE_NO_WARNINGS" }
+		excludes {
+			"./src/**/*posix*",
+			"./src/**/*ImageGeneratorCG*"
+		}
 	filter "system:macosx"
 		defines { "SK_BUILD_FOR_MAC" }
+		excludes {
+			"./src/**/*ImageGeneratorWIC*"
+		}
